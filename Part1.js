4c56621975272df9442e08651a51b68649a60375
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2021-12-15 11:16:54 +0100

    Made some actual progress

--- Part1.js
+++ Part1.js
@@ -1,10 +1,39 @@
 console.clear();
+const TotalAnimals = [];
 class Jungle {
   constructor(name) {
     this.name = name;
+    if (this instanceof Animal) {
+      TotalAnimals.push(this);
+    }
   }
   soundOff() {
-    return this;
+    console.log(TotalAnimals);
+    TotalAnimals.map((thisAnimal) => {
+      if (thisAnimal instanceof Monkey) {
+        //Check if the monkey has enough energy to make a sound
+        if (thisAnimal.energyLevel < 4) {
+          //Monkey cannot make a sound
+        } else {
+          console.log(
+            thisAnimal.makeSound(),
+            "Energy remaining: ",
+            thisAnimal.energyLevel
+          );
+        }
+      } else {
+        //Check if other animals can make sound
+        if (thisAnimal.energyLevel < 3) {
+          //Animal cannot make sound
+        } else {
+          console.log(
+            thisAnimal.makeSound(),
+            "Energy remaining: ",
+            thisAnimal.energyLevel
+          );
+        }
+      }
+    });
   }
 }
 class Animal extends Jungle {
@@ -14,12 +43,20 @@ class Animal extends Jungle {
   }
   makeSound() {
     if (this instanceof Monkey) {
-      this.energyLevel -= 4;
+      if (this.energyLevel < 4) {
+        return `This animal is too tired to make a sound!`;
+      } else {
+        this.energyLevel -= 4;
+        return `${this.name} made a sound!`;
+      }
     } else {
-      this.energyLevel -= 1;
+      if (this.energyLevel < 3) {
+        return `This animal is too tired to make a sound!`;
+      } else {
+        this.energyLevel -= 3;
+        return `${this.name} made a sound!`;
+      }
     }
-
-    return `${this.name} made a sound!`;
   }
   eatFood() {
     if (this instanceof Monkey) {
@@ -27,7 +64,7 @@ class Animal extends Jungle {
     } else {
       this.energyLevel += 5;
     }
-    return `${this.name} made a sound!`;
+    return `${this.name} is munching!`;
   }
   sleep() {
     if (this instanceof Tiger) {
@@ -35,7 +72,7 @@ class Animal extends Jungle {
     } else {
       this.energyLevel += 10;
     }
-    return `${this.name} is slepping!`;
+    return `${this.name} is sleeping!`;
   }
 }
 class Monkey extends Animal {
@@ -82,4 +119,8 @@ class Snake extends Animal {
     return Snake.SnakeCount;
   }
 }
-const j = new Monkey("Silas", 10);
+const j = new Monkey("Monkey", 10);
+const s = new Snake("Tempter", 10);
+const t = new Tiger("Tigger", 10);
+const Amazon = new Jungle("Amazon Rainforest");
+console.log(Amazon.soundOff());
